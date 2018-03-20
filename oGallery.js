19af8934f88d46ea13cd993c49f4236899f3b1ff
emuratov <emuratov@yandex.ru>
2018-03-20 20:04:59 +0700

    good

--- oGallery.js
+++ oGallery.js
@@ -1,18 +1,19 @@
-const widthSection = 1200;
+const widthSection = 1000;
+const itemsMaxHeight = 500;
 
-const itemsMaxHeight = 60;
-
-document.getElementById('oGallery').style.width = widthSection + 'px';
+window.onload = function () {
+    document.getElementById('oGallery').style.width = widthSection + 'px';
 
     let itemsAll = document.getElementsByClassName('oImg');
 
     for (let i = 0; i < (itemsAll.length) ; i++ ){
-        itemsAll[i].style.maxHeight = itemsMaxHeight + 'vh';
+        itemsAll[i].style.maxHeight = itemsMaxHeight + 'px';
+        itemsAll[i].style.paddingBottom = 2 + 'px';
     }
-
 
     let itemsCount =0;
     let count = 0;
+    let prewI = 0;
     for (let i = 0; i < (itemsAll.length) ; i++ ){
 
 
@@ -27,7 +28,25 @@ document.getElementById('oGallery').style.width = widthSection + 'px';
 
         } else
         {
+            let itemsMult = (widthSection -5) /itemsCount;
+            console.log('prewI', prewI);
+
+            for (let j = prewI; j < i ; j++){
+                itemsAll[j].style.maxHeight = (itemsMaxHeight * itemsMult) + 'px';
+            }
+
+            prewI = i;
+            console.log('prewI', prewI);
+
             itemsCount = 0;
             count = 0;
+            i = i-1;
         }
     }
+
+}
+
+
+
+
+
